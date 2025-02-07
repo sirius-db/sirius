f033432f7f6ceb4447b3fecc6d92646b6f3b0c84
Bobbi Yogatama <bwyogatama@gmail.com>
2025-02-07 05:04:33 +0000

    Adding queries for gh200

--- queries/tpch/working/q16.sql
+++ queries/tpch/working/q16.sql
@@ -23,34 +23,3 @@ group by
   p_brand,
   p_type,
   p_size
-
-
-select
-  p_brand,
-  p_type,
-  count(distinct ps_suppkey) as supplier_cnt,
-  p_size
-from
-  partsupp,
-  part
-where
-  p_partkey = ps_partkey
-  and p_brand <> 45
-  and (p_type < 65 or p_type >= 70)
-  and p_size in (49, 14, 23, 45, 19, 3, 36, 9)
-  and ps_suppkey not in (
-    select
-      s_suppkey
-    from
-      supplier
-    where
-      s_comment like '%Customer%Complaints%'
-  )
-group by
-  p_brand,
-  p_type,
-  p_size
-order by
-  p_brand,
-  p_type,
-  p_size;
