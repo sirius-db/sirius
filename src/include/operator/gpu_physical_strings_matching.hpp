59f849b845cc5c6f73f09c1e18369419fbbc9b40
Kevin Kristensen <kckristensen@wisc.edu>
2025-05-26 18:15:20 +0000

    Needed to expose string_split in header for use by expression_executor.

--- src/include/operator/gpu_physical_strings_matching.hpp
+++ src/include/operator/gpu_physical_strings_matching.hpp
@@ -5,6 +5,7 @@
 
 namespace duckdb {
 
+std::vector<std::string> string_split(std::string s, std::string delimiter);
 void StringMatching(char* char_data, uint64_t* str_indices, std::string match_string, uint64_t* &row_id, uint64_t* &count, uint64_t num_chars, uint64_t num_strings, int not_equal);
 void MultiStringMatching(char* char_data, uint64_t* str_indices, std::vector<std::string> all_terms,
        uint64_t* &row_id, uint64_t* &count, uint64_t num_chars, uint64_t num_string, int not_equal);
