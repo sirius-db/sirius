481d2b1df2abe1e0864bb3678c098d8522b4a1d2
Felipe Aramburu <felipearamburu@gmail.com>
2026-01-02 10:46:13 -0600

    fixing building cucascade

--- third_party/cucascade.cmake
+++ third_party/cucascade.cmake
@@ -25,8 +25,10 @@ ExternalProject_Add(
   GIT_SHALLOW ON
   UPDATE_DISCONNECTED TRUE
   BUILD_BYPRODUCTS ${CUCASCADE_STATIC_LIB}
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} --preset release -S <SOURCE_DIR> -B
-                    <SOURCE_DIR>/build/release
+  CONFIGURE_COMMAND
+    ${CMAKE_COMMAND} -E env "CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"
+    ${CMAKE_COMMAND} --preset release -S <SOURCE_DIR> -B
+    <SOURCE_DIR>/build/release
   BUILD_COMMAND ${CMAKE_COMMAND} --build <SOURCE_DIR>/build/release
   INSTALL_COMMAND ""
   BUILD_IN_SOURCE FALSE)
@@ -34,11 +36,15 @@ ExternalProject_Add(
 file(MAKE_DIRECTORY ${CUCASCADE_INCLUDE_DIR}) # Include directory needs to exist
                                               # to run configure
 
+# Find fmt which is required by cuCascade
+find_package(fmt REQUIRED CONFIG)
+
 add_library(cucascade::cucascade STATIC IMPORTED)
 set_target_properties(cucascade::cucascade PROPERTIES IMPORTED_LOCATION
                                                       ${CUCASCADE_STATIC_LIB})
 target_include_directories(cucascade::cucascade
                            INTERFACE ${CUCASCADE_INCLUDE_DIR})
-# cuCascade uses NUMA-aware memory allocation
-set_property(TARGET cucascade::cucascade PROPERTY INTERFACE_LINK_LIBRARIES numa)
+# cuCascade uses NUMA-aware memory allocation and fmt for string formatting
+set_property(TARGET cucascade::cucascade PROPERTY INTERFACE_LINK_LIBRARIES
+             numa fmt::fmt)
 add_dependencies(cucascade::cucascade ${CUCASCADE_BASE})
