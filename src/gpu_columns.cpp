207fb9c83d4ca1edba8d6e5261adf281549e799f
HermesCui <cuichihan@gmail.com>
2026-01-27 01:13:46 +0000

    refactor codebase

--- src/gpu_columns.cpp
+++ src/gpu_columns.cpp
@@ -93,7 +93,6 @@ size_t DataWrapper::getColumnTypeSize() const
       GPUDecimalTypeInfo* decimal_type_info = type.GetDecimalTypeInfo();
       if (decimal_type_info == nullptr) throw InternalException(
           "`decimal_type_info` not set for DECIMAL type in `getColumnTypeSize`");
-      }
       return decimal_type_info->GetDecimalTypeSize();
     }
     default:
@@ -405,53 +404,78 @@ void GPUColumn::setFromCudfScalar(cudf::scalar& cudf_scalar, GPUBufferManager* g
   size_t scalar_size          = 0;
 
   if (scalar_type == cudf::data_type(cudf::type_id::INT64)) {
-    auto& s           = static_cast<cudf::numeric_scalar<uint64_t>&>(cudf_scalar);
+    auto& s           = static_cast<cudf::numeric_scalar<int64_t>&>(cudf_scalar);
     scalar_ptr        = s.data();
-    scalar_size       = 8;
+    scalar_size       = sizeof(int64_t);
     data_wrapper.type = GPUColumnType(GPUColumnTypeId::INT64);
   } else if (scalar_type == cudf::data_type(cudf::type_id::INT32)) {
     auto& s           = static_cast<cudf::numeric_scalar<int32_t>&>(cudf_scalar);
     scalar_ptr        = s.data();
-    scalar_size       = 4;
+    scalar_size       = sizeof(int32_t);
     data_wrapper.type = GPUColumnType(GPUColumnTypeId::INT32);
-  } else if (scalar_type ==
-             cudf::data_type(cudf::type_id::INT16)) {  // [Fix] Add INT16 scalar support
+  } else if (scalar_type == cudf::data_type(cudf::type_id::INT16)) {
     auto& s           = static_cast<cudf::numeric_scalar<int16_t>&>(cudf_scalar);
     scalar_ptr        = s.data();
-    scalar_size       = 2;
+    scalar_size       = sizeof(int16_t);
     data_wrapper.type = GPUColumnType(GPUColumnTypeId::INT16);
   } else if (scalar_type == cudf::data_type(cudf::type_id::FLOAT32)) {
     auto& s           = static_cast<cudf::numeric_scalar<float>&>(cudf_scalar);
     scalar_ptr        = s.data();
-    scalar_size       = 4;
+    scalar_size       = sizeof(float);
     data_wrapper.type = GPUColumnType(GPUColumnTypeId::FLOAT32);
   } else if (scalar_type == cudf::data_type(cudf::type_id::FLOAT64)) {
     auto& s           = static_cast<cudf::numeric_scalar<double>&>(cudf_scalar);
     scalar_ptr        = s.data();
-    scalar_size       = 8;
+    scalar_size       = sizeof(double);
     data_wrapper.type = GPUColumnType(GPUColumnTypeId::FLOAT64);
   } else if (scalar_type == cudf::data_type(cudf::type_id::BOOL8)) {
     auto& s           = static_cast<cudf::numeric_scalar<bool>&>(cudf_scalar);
     scalar_ptr        = s.data();
-    scalar_size       = 1;
+    scalar_size       = sizeof(bool);
     data_wrapper.type = GPUColumnType(GPUColumnTypeId::BOOLEAN);
   } else if (scalar_type.id() == cudf::type_id::DECIMAL32) {
     auto& s           = static_cast<cudf::fixed_point_scalar<numeric::decimal32>&>(cudf_scalar);
     scalar_ptr        = s.data();
-    scalar_size       = 4;
+    scalar_size       = sizeof(int32_t);
     data_wrapper.type = GPUColumnType(GPUColumnTypeId::DECIMAL);
     data_wrapper.type.SetDecimalTypeInfo(Decimal::MAX_WIDTH_INT32, -s.type().scale());
   } else if (scalar_type.id() == cudf::type_id::DECIMAL64) {
     auto& s           = static_cast<cudf::fixed_point_scalar<numeric::decimal64>&>(cudf_scalar);
     scalar_ptr        = s.data();
-    scalar_size       = 8;
+    scalar_size       = sizeof(int64_t);
     data_wrapper.type = GPUColumnType(GPUColumnTypeId::DECIMAL);
     data_wrapper.type.SetDecimalTypeInfo(Decimal::MAX_WIDTH_INT64, -s.type().scale());
   } else if (scalar_type.id() == cudf::type_id::TIMESTAMP_DAYS) {
     auto& s           = static_cast<cudf::numeric_scalar<int32_t>&>(cudf_scalar);
     scalar_ptr        = s.data();
-    scalar_size       = 4;
+    scalar_size       = sizeof(int32_t);
     data_wrapper.type = GPUColumnType(GPUColumnTypeId::DATE);
+  } else if (scalar_type.id() == cudf::type_id::STRING) {
+    auto& typed_scalar = static_cast<cudf::string_scalar&>(cudf_scalar);
+    size_t string_size = typed_scalar.size();
+    if (string_size > 0) {
+      data_wrapper.data = gpuBufferManager->customCudaMalloc<uint8_t>(string_size, 0, 0);
+      callCudaMemcpyDeviceToDevice<uint8_t>(
+        data_wrapper.data,
+        const_cast<uint8_t*>(reinterpret_cast<const uint8_t*>(typed_scalar.data())),
+        string_size,
+        0);
+    } else {
+      data_wrapper.data = nullptr;
+    }
+    data_wrapper.type           = GPUColumnType(GPUColumnTypeId::VARCHAR);
+    data_wrapper.num_bytes      = string_size;
+    data_wrapper.offset         = gpuBufferManager->customCudaMalloc<uint64_t>(2, 0, 0);
+    uint64_t offsets[]          = {0, string_size};
+    callCudaMemcpyHostToDevice<uint64_t>(data_wrapper.offset, offsets, 2, 0);
+    data_wrapper.is_string_data = true;
+    data_wrapper.size           = 1;
+    data_wrapper.validity_mask  = createNullMask(1);
+    data_wrapper.mask_bytes     = getMaskBytesSize(1);
+    column_length               = 1;
+    row_ids                     = nullptr;
+    row_id_count                = 0;
+    return;
   } else {
     throw NotImplementedException("Unsupported scalar type: %d",
                                   static_cast<int>(scalar_type.id()));
@@ -509,8 +533,18 @@ GPUIntermediateRelation::GPUIntermediateRelation(size_t column_count) : column_c
 
 bool GPUIntermediateRelation::checkLateMaterialization(size_t idx)
 {
-  SIRIUS_LOG_DEBUG("Checking late mat idx {}", idx);
-  if (columns[idx] == nullptr) return false;
+  SIRIUS_LOG_DEBUG(
+    "Checking if column idx {} needs to be materialized from column size {}", idx, columns.size());
+  if (columns[idx] == nullptr) {
+    SIRIUS_LOG_DEBUG("Column idx {} is null", idx);
+    return false;
+  }
+
+  if (columns[idx]->row_ids == nullptr) {
+    SIRIUS_LOG_DEBUG("Column idx {} already materialized", idx);
+  } else {
+    SIRIUS_LOG_DEBUG("Column idx {} needs to be materialized", idx);
+  }
   return columns[idx]->row_ids != nullptr;
 }
 
