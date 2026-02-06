bf2d0cd411aa15a80d2fa4f3e94ba6c28b40929f
William Malpica <wmalpica@gmail.com>
2026-02-05 20:50:15 -0600

    Addressing review comment

--- src/sirius_engine.cpp
+++ src/sirius_engine.cpp
@@ -748,7 +748,7 @@ void sirius_engine::initialize_internal(op::sirius_physical_operator& plan)
       if (new_scheduled[i]->sink.get()) {
         for (auto& [next_op, port_id] : new_scheduled[i]->sink.get()->get_next_port_after_sink()) {
           new_scheduled[i]->parents.push_back(
-            duckdb::weak_ptr<sirius::pipeline::sirius_pipeline>(next_op->get_pipeline()));
+            duckdb::weak_ptr<sirius::pipeline::sirius_pipeline>(next_op->get_port(port_id)->dest_pipeline));
         }
       }
     }
