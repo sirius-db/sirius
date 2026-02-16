632f7f8fd4f79f8a76f194217b3ca29df04980f3
Joost Hoozemans <joosthooz@msn.com>
2026-02-16 12:35:29 +0100

    Checking the result of a dynamic cast

--- src/op/sirius_physical_concat.cpp
+++ src/op/sirius_physical_concat.cpp
@@ -18,7 +18,6 @@
 
 #include "duckdb/planner/expression/bound_reference_expression.hpp"
 #include "expression_executor/gpu_expression_executor.hpp"
-#include "log/logging.hpp"
 #include "op/merge/gpu_merge_impl.hpp"
 #include "op/sirius_physical_grouped_aggregate.hpp"
 #include "op/sirius_physical_hash_join.hpp"
@@ -159,7 +158,13 @@ void sirius_physical_concat::sink(const operator_data& output_data, rmm::cuda_st
     for (auto& [next_op, port_id] : next_port_after_sink) {
       auto partition_consumer_op =
         dynamic_cast<sirius_physical_partition_consumer_operator*>(next_op);
-      partition_consumer_op->push_data_batch_partitioned(port_id, batch, partition_idx);
+      if (partition_consumer_op) {
+        partition_consumer_op->push_data_batch_partitioned(port_id, batch, partition_idx);
+      } else {
+        throw std::runtime_error(
+          "sirius_physical_concat::sink(): Next operator is not a partition consumer operator: " +
+          SiriusPhysicalOperatorToString(next_op->type));
+      }
     }
   }
 }
