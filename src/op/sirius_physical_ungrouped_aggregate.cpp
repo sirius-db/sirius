a6bfe5fd8c0a39fc7c7fadace7f04dcd245b34f2
Joost Hoozemans <joosthooz@msn.com>
2026-02-23 18:19:52 +0100

    Upcasting decimal before the reduction in local ungrouped aggregate

--- src/op/sirius_physical_ungrouped_aggregate.cpp
+++ src/op/sirius_physical_ungrouped_aggregate.cpp
@@ -395,6 +395,14 @@ std::unique_ptr<operator_data> sirius_physical_ungrouped_aggregate::execute(
           bool is_decimal = (col.type().id() == cudf::type_id::DECIMAL32 ||
                              col.type().id() == cudf::type_id::DECIMAL64 ||
                              col.type().id() == cudf::type_id::DECIMAL128);
+          if (col.type().id() == cudf::type_id::DECIMAL32) {
+            col = *cudf::cast(
+              col, cudf::data_type(cudf::type_id::DECIMAL64, col.type().scale()), stream);
+          }
+          if (col.type().id() == cudf::type_id::DECIMAL64) {
+            col = *cudf::cast(
+              col, cudf::data_type(cudf::type_id::DECIMAL128, col.type().scale()), stream);
+          }
           if (spec.kind == aggregate_kind::AVG || is_decimal) { out_type = col.type(); }
           auto scalar = cudf::reduce(col, *agg_op, out_type, std::nullopt, stream);
           cols.push_back(cudf::make_column_from_scalar(*scalar, 1, stream));
