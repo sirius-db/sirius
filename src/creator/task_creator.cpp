a93b5327f9a423fd3125b7560af8c4ec39b8fec1
Joost Hoozemans <joosthooz@msn.com>
2026-03-18 16:47:28 +0100

    Only schedule all tasks for a scan if there is just a single scan in the plan

--- src/creator/task_creator.cpp
+++ src/creator/task_creator.cpp
@@ -286,6 +286,8 @@ void task_creator::manager_loop()
           size_t operator_id             = node->get_operator_id();
           auto parquet_task_global_state = _parquet_scan_operator_global_state_map.at(operator_id);
           auto* parquet_scan             = &node->Cast<op::sirius_physical_parquet_scan>();
+          size_t const num_scans_in_plan =
+            _scan_operator_global_state_map.size() + _parquet_scan_operator_global_state_map.size();
           while (true) {
             pipeline->mark_task_created();
             auto const partition_idx = parquet_task_global_state->get_next_rg_partition_idx();
@@ -317,6 +319,10 @@ void task_creator::manager_loop()
                                                             std::move(parquet_task_local_state),
                                                             parquet_task_global_state);
             _pipeline_executor->schedule(std::move(parquet_task));
+
+            // If there is only a single scan in the plan, continue creating scan tasks to create
+            // I/O parallelism. Otherwise, let the plan drive the creation of more tasks.
+            if (num_scans_in_plan >= 2) { break; }
           }
         } else {
           // need to exhaust input batches until all ports are empty
