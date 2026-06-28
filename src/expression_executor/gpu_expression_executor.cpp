ffc115f32c67773fd3b7d4872f8c1c63fcc742b5
Kevin Kristensen <kkristensen@nvidia.com>
2026-06-28 22:27:30 +0000

    Remove TODO comment.

--- src/expression_executor/gpu_expression_executor.cpp
+++ src/expression_executor/gpu_expression_executor.cpp
@@ -48,8 +48,6 @@
 #include <numeric>
 #include <variant>
 
-/// TODO: expose bitmap-returning API
-
 namespace {
 /// Returns true if cudf::cast supports this type (fixed-width only; no STRING/LIST/STRUCT/etc.).
 bool IsFixedWidth(cudf::data_type const& type)
