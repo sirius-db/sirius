7d66252bc6bfb146aa6d6c645ec784230626a2ee
Kevin Kristensen <kkristensen@nvidia.com>
2026-07-07 16:54:33 +0000

    Format cuco vcpkg ports.

--- vcpkg_ports/cuco/cuco-config.cmake
+++ vcpkg_ports/cuco/cuco-config.cmake
@@ -2,6 +2,7 @@
 # Thrust) comes from cudf, so this target intentionally has no CCCL dependency.
 if(NOT TARGET cuco::cuco)
   add_library(cuco::cuco INTERFACE IMPORTED)
-  set_target_properties(cuco::cuco PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/../../include")
+  set_target_properties(
+    cuco::cuco PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
+                          "${CMAKE_CURRENT_LIST_DIR}/../../include")
 endif()
