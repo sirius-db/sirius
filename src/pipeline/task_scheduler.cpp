620e524966fa8acd5cf4a4ee1fa83ba0499f5a02
Amin Aramoon <aaramoon@nvidia.com>
2026-07-15 15:09:56 -0700

    check executor is not null

--- src/pipeline/task_scheduler.cpp
+++ src/pipeline/task_scheduler.cpp
@@ -317,7 +317,7 @@ void task_scheduler::management_eventloop()
     }
 
     if (_task_queue.is_empty()) {
-      if (!_task_creator) { _task_creator->schedule_lookahead(*_ready_devices.begin()); }
+      if (_task_creator) { _task_creator->schedule_lookahead(*_ready_devices.begin()); }
     }
 
     // Matcher: for each ready device, try to find a dispatchable task.
