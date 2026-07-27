1ec3adde0427c1d643e040ba9fa143c1cd9face2
Mike Wendt <1915404+mike-wendt@users.noreply.github.com>
2026-07-27 15:43:10 -0400

    TST Add error for linter

--- src/sirius_engine.cpp
+++ src/sirius_engine.cpp
@@ -86,9 +86,7 @@ sirius_engine::sirius_engine(duckdb::ClientContext& context, sirius_interface& s
 
 sirius_engine::~sirius_engine() { query_handle_->exit(); }
 
-void sirius_engine::reset()
-{
-  sirius_physical_plan = nullptr;
+void sirius_engine::reset(){sirius_physical_plan=nullptr; // CI test: clang-format me
   sirius_owned_plan.reset();
   sirius_root_pipelines.clear();
   root_pipeline_idx = 0;
