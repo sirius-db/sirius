498930d17f9733be716d3a0919236c2d691ed5d4
James Bourbeau <jbourbeau@nvidia.com>
2026-09-10 12:30:10 -0500

    Lint
    Signed-off-by: James Bourbeau <jbourbeau@nvidia.com>

--- src/expression_evaluator/specializations/function.cpp
+++ src/expression_evaluator/specializations/function.cpp
@@ -186,13 +186,12 @@ evaluate_result expression_evaluator::evaluate(sirius::ast::function_call const&
 
     auto const input_strings = cudf::strings_column_view(input.get_column_view());
 #if CUDF_VERSION_NUM >= 2610
-    auto result_column = cudf::strings::slice_strings(
-      input_strings,
-      std::optional<cudf::size_type>{start_val},
-      std::optional<cudf::size_type>{stop_val},
-      std::optional<cudf::size_type>{1},
-      _stream,
-      _mr);
+    auto result_column = cudf::strings::slice_strings(input_strings,
+                                                      std::optional<cudf::size_type>{start_val},
+                                                      std::optional<cudf::size_type>{stop_val},
+                                                      std::optional<cudf::size_type>{1},
+                                                      _stream,
+                                                      _mr);
 #else
     auto result_column =
       cudf::strings::slice_strings(input_strings,
